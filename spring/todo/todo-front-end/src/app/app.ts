3351cd16707ad5375e4566c5bb97b6281c162f2a
Nicholas Wilfong <nicholaswilfong93@gmail.com>
2026-06-24 16:24:30 -0400

    Generate home, task, subtask components

--- spring/todo/todo-front-end/src/app/app.ts
+++ spring/todo/todo-front-end/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//Component imports
+import { Login } from '.login/login';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
+  //used as a signal in case we wanted to change the webpages title dyamically 
+  //at some point
   protected readonly title = signal('todo-back-end');
 }
---
+++ spring/todo/todo-front-end/src/app/subtask/subtask.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtask',
+  imports: [],
+  templateUrl: './subtask.html',
+  styleUrl: './subtask.css',
+})
+export class Subtask {
+
+}
