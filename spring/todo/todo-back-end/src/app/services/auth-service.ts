bcb7dc03935a17f424725881bc2f8cbad8f79ba0
David Allen Torrijos Santos <davidallen0717@yahoo.com>
2026-06-24 12:41:13 -0400

    Starting to add functionality to the AuthService, but I am trying to fix errors relating to Router and injection

--- spring/todo/todo-back-end/src/app/services/auth-service.ts
+++ spring/todo/todo-back-end/src/app/services/auth-service.ts
@@ -6,5 +6,23 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AuthService {
-  
+    // if you ever need to control routing in your classes use the Router service
+  private router = inject(Router)
+
+  // this is our placeholder for a real authentication token
+  private isLoggedIn = false;
+
+  checkIfUserIsLoggedIn(){
+    return this.isLoggedIn;
+  }
+
+  /*
+    Note: in a real login attempt you would want to actually send the user
+          credentials to the server and validate them. Here we will just
+          toggle theisLoggedIn flag
+  */
+  attemptLogin(){
+    this.isLoggedIn = true;
+    this.router.navigate(["home"]);
+  }
 }
