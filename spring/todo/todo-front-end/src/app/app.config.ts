60aa1048c27c6c3df3196bd9a9daf5636825a976
Nicholas Wilfong <nicholaswilfong93@gmail.com>
2026-06-24 15:58:24 -0400

    Basic start for the Routes array and app config

--- spring/todo/todo-front-end/src/app/app.config.ts
+++ spring/todo/todo-front-end/src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './auth/authInterceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
+
+    //here I have included HTTPClient because we are using an interceptor
+    //with route guards
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
--- spring/todo/todo-front-end/src/app/app.routes.ts
+++ spring/todo/todo-front-end/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//potential routing path imports, TO BE ADJUSTED LATER
+import { Login } from '.login/login';
+import { Register } from './register/register';
+import { Home } from './home/home';
+import {authGuard } from '/auth/auth.guard';
+
+
+
+export const routes: Routes = [
+
+    //forwarding from /root path to login
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+    //path to login component
+    {path: 'login', component: Login},
+    
+    //path to register component
+    {path: 'register', component: Register},
+
+    //path to home page view, with route guard
+    //{path: 'home', component: Home, canActivate: [authGuard]}
+
+    //path to tasks view, with route guard
+    {path: 'task', component: Task, canActivate: [authGuard]},
+    
+    //path to subtask view, with route guard
+    {path: 'subtask', component: Subtask, canActivate: [authGuard]},
+];
