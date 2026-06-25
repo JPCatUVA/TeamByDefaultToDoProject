0f35d15410513ef843294c556f49c214617d3846
Nicholas Wilfong <nicholaswilfong93@gmail.com>
2026-06-25 09:29:33 -0400

    Kiro error fixes

--- spring/todo/todo-front-end/src/app/home/home.ts
+++ spring/todo/todo-front-end/src/app/home/home.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//This will be the main view of the application
+//It will display a list of all the logged in users tasks
+//and allow them to bring one to the forefront
+
+//DOM adjustments are handled through RxJs Observables
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
