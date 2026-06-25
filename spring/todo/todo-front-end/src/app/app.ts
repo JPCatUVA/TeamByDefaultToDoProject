878d6e1fc3900dd5a59d62f58568812205671c6a
Nicholas Wilfong <nicholaswilfong93@gmail.com>
2026-06-25 12:48:56 -0400

    Added logout button to all relevant pages

--- spring/todo/todo-front-end/src/app/app.ts
+++ spring/todo/todo-front-end/src/app/app.ts
@@ -1,18 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-//Component imports
-//import { Login } from '.login/login';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],//, Login],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
-  //used as a signal in case we wanted to change the webpages title dyamically 
-  //at some point
+  private authService = inject(AuthService);
+
+  // used as a signal in case we wanted to change the webpage's title dynamically
   protected readonly title = signal('todo-back-end');
+
+  get isLoggedIn(): boolean {
+    return this.authService.checkIfUserIsLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
