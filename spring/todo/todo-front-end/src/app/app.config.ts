cb263f5dd76c0a6bcbee5e1af99be79876e379a8
Nicholas Wilfong <nicholaswilfong93@gmail.com>
2026-06-25 10:19:40 -0400

    changed routes to use correct variable names

--- spring/todo/todo-front-end/src/app/app.config.ts
+++ spring/todo/todo-front-end/src/app/app.config.ts
@@ -3,15 +3,15 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { authInterceptor } from './auth/authInterceptor';
+import { loginAuthGuard } from './guards/login-auth-guard';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
 
     //here I have included HTTPClient because we are using an interceptor
     //with route guards
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([loginAuthGuard]))
   ]
 };
