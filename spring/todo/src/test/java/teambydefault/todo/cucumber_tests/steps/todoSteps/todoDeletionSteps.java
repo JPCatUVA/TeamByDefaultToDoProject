b99086436a6965cc168dab0f024e711af00459d9
Nick Wilfong <nicholaswilfong93@gmail.com>
2026-07-16 04:56:24 -0400

    removed unused import

--- spring/todo/src/test/java/teambydefault/todo/cucumber_tests/steps/todoSteps/todoDeletionSteps.java
+++ spring/todo/src/test/java/teambydefault/todo/cucumber_tests/steps/todoSteps/todoDeletionSteps.java
@@ -2,7 +2,6 @@ package teambydefault.todo.cucumber_tests.steps.todoSteps;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
